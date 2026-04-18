f620ab0af90e30c4f84a38733418f6be04283daa
AldwinDukes <aldwinwebdevstudy@gmail.com>
2026-04-18 14:04:41 +0800

    Added the routers tag

---
+++ src/components/FormAdd.jsx
@@ -0,0 +1,9 @@
+function FormAdd() {
+  return (
+    <>
+      <h1 className="font-extrabold">Form Add</h1>
+    </>
+  );
+}
+
+export default FormAdd;
---
+++ src/components/Upload.jsx
@@ -0,0 +1,9 @@
+function Upload() {
+  return (
+    <>
+      <h1 className="font-extrabold">Upload</h1>
+    </>
+  );
+}
+
+export default Upload;
